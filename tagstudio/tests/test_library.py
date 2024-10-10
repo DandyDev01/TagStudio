6fd64a015d5bb6e854de6fd5447711c910c95f45
dandydev01 <sandermankelsey@gmail.com>
2024-10-10 13:06:39 -0600

    [test] added test_library_get_alias

--- tagstudio/tests/test_library.py
+++ tagstudio/tests/test_library.py
@@ -18,9 +18,24 @@ def test_library_add_alias(library, generate_tag):
     alias_names.add("test_alias")
     library.update_tag(tag, subtag_ids, alias_names, alias_ids)
 
+    # Note: ask if it is expected behaviour that you need to re-request 
+    #       for the tag. Or if the tag in memory should be updated
     alias_ids = library.get_tag(tag.id).alias_ids
 
     assert len(alias_ids) == 1
+    
+def test_library_get_alias(library, generate_tag):
+    tag = library.add_tag(generate_tag("xxx", id=123))
+    assert tag
+
+    subtag_ids: set[int] = set()
+    alias_ids: set[int] = set()
+    alias_names: set[str] = set()
+    alias_names.add("test_alias")
+    library.update_tag(tag, subtag_ids, alias_names, alias_ids)
+
+    alias_ids = library.get_tag(tag.id).alias_ids
+
     assert library.get_alias(tag.id, alias_ids[0]).name == "test_alias"
 
 def test_library_bootstrap():
